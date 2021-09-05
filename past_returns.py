da3ad74e0cf956d46a6f90ee46d6d305a29024fb
“mk0417” <pengdata1@gmail.com>
2021-09-05 22:50:00 +0100

    refactor

--- past_returns.py
+++ past_returns.py
@@ -1,100 +1,115 @@
 # -------------------------------------------------------------------------
 #                       Past n-month return
 #
-# Calculate return for past n-month
-# For example, you can calculate past 6-month return by setting
-# `j` (horizon )to 6
-# For momentum, literature usually skip the most recent month when
-# computing past returns, and this feature is set by default. Please
-# set `skip` to False if you do not want to skip
+# pre3ret: past 3-month return
+# pre6ret: past 6-month return
+# pre9ret: past 9-month return
+# pre12ret: past 12-month return
+# pre12_7ret: past 12 to 7 month return
+#
+# All calculations skip current month
+# Require at least n-1 months in past n months
 # -------------------------------------------------------------------------
 
-
+import wrds
+import configparser as cp
 import pandas as pd
 import numpy as np
+import os
+import time
 
+class ap_preret:
+    def __init__(self):
+        start_time = time.time()
+        pass_dir = '~/.pass'
+        cfg = cp.ConfigParser()
+        cfg.read(os.path.join(os.path.expanduser(pass_dir), 'credentials.cfg'))
+        conn = wrds.Connection(wrds_username=cfg['wrds']['username'])
 
-class ap_pre_ret:
-    def __init__(self, fpath):
-        vars_list = ['permno', 'date', 'ret',
-            'prc', 'shrout', 'exchcd', 'shrcd']
-        df = pd.read_parquet(fpath+'msf.parquet.gzip', columns=vars_list)
-        df = df.query('-2<=exchcd<=3 & shrcd==[10, 11]').copy()
-        df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
-        df['date'] = df['date'] + pd.offsets.MonthEnd(0)
-        df['yyyymm'] = df['date'].dt.year * 100 + df['date'].dt.month
-        df = df.drop_duplicates(['permno', 'yyyymm'], keep='last')
-        df['price'] = df['prc'].abs()
-        df.loc[df['price']<=0, 'price'] = np.nan
-        df['me'] = df['price'] * df['shrout'] / 1000
-        df.loc[df['me']<=0, 'me'] = np.nan
-        df['month_idx'] = ((df['date'].dt.year-1925) * 12
-            + df['date'].dt.month - 11)
-        df.loc[df['ret']<=-1, 'ret'] = np.nan
-        df = df.drop('prc', axis=1).copy()
-        df = df.sort_values(['permno', 'yyyymm'], ignore_index=True)
-        self.df = df.copy()
+        # Extract CRSP daily data
+        msf = conn.raw_sql("""
+            select a.permno, a.date, a.ret
+            from crsp.msf a left join crsp.msenames b
+                on a.permno=b.permno and a.date>=b.namedt and a.date<=b.nameendt
+            where b.exchcd between -2 and 3 and b.shrcd between 10 and 11
+        """, date_cols=['date'])
 
-    def pre_ret(self, j, skip=True):
-        """
-        j: formation periods: past j month
-        skip: bool, default True
-            True to skip most recent month
-            False to include most recent month
-        """
-        df = self.df.copy()
+        msf['permno'] = msf['permno'].astype(int)
+        msf['date'] = msf['date'] + pd.offsets.MonthEnd(0)
+        msf['yyyymm'] = msf['date'].dt.year*100 + msf['date'].dt.month
+        msf = msf.drop_duplicates(['permno', 'yyyymm'])
+        msf['midx'] = ((msf['date'].dt.year-1925) * 12
+            + msf['date'].dt.month - 11)
+        msf.loc[msf['ret']<=-1, 'ret'] = np.nan
+        msf = msf.sort_values(['permno', 'yyyymm'], ignore_index=True)
+        self.msf = msf.copy()
+
+        end_time = time.time()
+        print('\n--------- Extract data from WRDS ---------')
+        print(f'time_used: {end_time-start_time: 3.1f} seconds\n')
+
+    def preret_est(self, j):
+        start_time = time.time()
+        df = self.msf.copy()
+
         df['logret'] = np.log(1+df['ret'])
         df = df.sort_values(['permno', 'yyyymm'], ignore_index=True)
-        df['l_logret'] = df.groupby('permno')['logret'].shift(1)
-        if skip:
-            df['pre'+str(j)+'ret'] = (df.groupby('permno')['l_logret']
-                .rolling(window=j, min_periods=j-1)
-                .sum().reset_index(drop=True))
-            df['l_month_idx'] = df.groupby('permno')['month_idx'].shift(j)
-            df['month_gap'] = df['month_idx'] - df['l_month_idx']
-            df['pre'+str(j)+'ret'] = np.exp(df['pre'+str(j)+'ret']) - 1
-            df.loc[df['month_gap']!=j, 'pre'+str(j)+'ret'] = np.nan
-            df = df[df['pre'+str(j)+'ret'].notna()].copy()
-            df = df[['permno', 'yyyymm', 'pre'+str(j)+'ret']]
-        else:
-            df['pre'+str(j)+'ret_noskip'] = (df.groupby('permno')['logret']
-                .rolling(window=j, min_periods=j-1)
-                .sum().reset_index(drop=True))
-            df['l_month_idx'] = df.groupby('permno')['month_idx'].shift(j-1)
-            df['month_gap'] = df['month_idx'] - df['l_month_idx']
-            df['pre'+str(j)+'ret_noskip'] = (
-                np.exp(df['pre'+str(j)+'ret_noskip']) - 1)
-            df.loc[df['month_gap']!=j-1, 'pre'+str(j)+'ret_noskip'] = np.nan
-            df = df[df['pre'+str(j)+'ret_noskip'].notna()].copy()
-            df = df[['permno', 'yyyymm', 'pre'+str(j)+'ret_noskip']]
-        return df
-
-    def pre_ret_out(self, j, skip):
-        """
-        j: list
-            formation periods: past j month
-        skip: list
-            True to skip most recent month
-            False to include most recent month
-        """
-        df = pd.DataFrame(columns=['permno', 'yyyymm'])
-        for m, n in zip(j, skip):
-            _tmp = self.pre_ret(m, n)
-            df = df.merge(_tmp, how='outer', on=['permno', 'yyyymm'])
-
+        df['l1logret'] = df.groupby('permno')['logret'].shift(1)
+        # Past n-month returns with at least n-1 months
+        df['pre'+str(j)+'ret'] = (df.groupby('permno')['l1logret']
+            .rolling(window=j, min_periods=j-1)
+            .sum().reset_index(drop=True))
+        df['lmidx'] = df.groupby('permno')['midx'].shift(j)
+        df['month_gap'] = df['midx'] - df['lmidx']
+        df['pre'+str(j)+'ret'] = np.exp(df['pre'+str(j)+'ret']) - 1
+        # 137 obs with month gaps
+        # Set to missing if there is month gap
+        df.loc[df['month_gap']!=j, 'pre'+str(j)+'ret'] = np.nan
+        df = df[df['pre'+str(j)+'ret'].notna()].copy()
+        df = df[['permno', 'yyyymm', 'pre'+str(j)+'ret']]
+        obs = len(df)
+        start_month = df['yyyymm'].min()
         df = df.sort_values(['permno', 'yyyymm'], ignore_index=True)
+
+        end_time = time.time()
+        print(f'--------- Past {j}-month return ---------')
+        print(f'Obs: {obs}')
+        print(f'Start month: {start_month}')
+        print(f'time_used: {end_time-start_time: 3.1f} seconds')
         return df
 
+    def pre12_7ret_est(self):
+        start_time = time.time()
+        print('\n--------- Past 12-to-7 return ---------')
+        pre6ret = self.preret_est(6)
+        pre12ret = self.preret_est(12)
 
-# Initialize object
-db = ap_pre_ret('/Users/ml/Data/wrds/parquet/')
+        df = pre6ret.merge(pre12ret, how='inner', on=['permno', 'yyyymm'])
+        df['pre12_7ret'] = np.log(1+df['pre12ret']) - np.log(1+df['pre6ret'])
+        df['pre12_7ret'] = np.exp(df['pre12_7ret']) - 1
+        df = (df.query('pre12_7ret==pre12_7ret')
+            [['permno', 'yyyymm', 'pre12_7ret']].copy())
+        obs = len(df)
+        start_month = df['yyyymm'].min()
+        df = df.sort_values(['permno', 'yyyymm'], ignore_index=True)
 
-# Past 6-month return skipping most recent month
-pre6ret = db.pre_ret(6)
+        end_time = time.time()
+        print(f'Obs: {obs}')
+        print(f'Start month: {start_month}')
+        print(f'time_used: {end_time-start_time: 3.1f} seconds\n')
+        return df
 
-# Past 6-month return including most recent month
-pre6ret_noskip = db.pre_ret(6, False)
+if __name__ == '__main__':
+    db = ap_preret()
+    preret = db.preret_est(3)
+    for i in [6, 9, 12]:
+        tmp = db.preret_est(i)
+        preret = preret.merge(tmp, how='left', on=['permno', 'yyyymm'])
 
-# Different formation horizons
-pret = db.pre_ret_out([6, 12, 6, 12], [True, True, False, False])
+    tmp = db.pre12_7ret_est()
+    preret = preret.merge(tmp, how='left', on=['permno', 'yyyymm'])
+    preret = preret.sort_values(['permno', 'yyyymm'], ignore_index=True)
+    data_dir = '/Volumes/Seagate/asset_pricing_data'
+    preret.to_csv(os.path.join(data_dir, 'preret.txt'), sep='\t', index=False)
+    print('Done: data is generated')
 
